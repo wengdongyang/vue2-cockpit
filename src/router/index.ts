d6d841c814cde94cd10855548e24b10e4c906024
wdy <wdy@pro.com>
2024-07-22 10:47:00 +0800

    feat：init

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,10 +1,10 @@
 import Vue from 'vue'
-import VueRouter from 'vue-router'
+import VueRouter, { RouteConfig } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 
 Vue.use(VueRouter)
 
-const routes = [
+const routes: Array<RouteConfig> = [
   {
     path: '/',
     name: 'home',
