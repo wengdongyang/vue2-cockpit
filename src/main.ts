8b1b2df98586d0506695f9f7e012efd40f26bca5
wdy <wdy@pro.com>
2024-07-22 12:26:23 +0800

    feat：init

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,22 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import 'minireset.css/minireset.css';
 
-Vue.config.productionTip = false
+import 'nprogress/nprogress.css';
+import 'font-awesome/css/font-awesome.css';
+
+import 'element-ui/lib/theme-chalk/index.css';
+import 'element-theme-dark';
+
+import Vue from 'vue';
+import Element from 'element-ui';
+import App from './App.vue';
+import router from './router';
+import store from './store';
+
+Vue.use(Element);
+Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
-  render: h => h(App)
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount('#app');
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,22 @@
-const { defineConfig } = require('@vue/cli-service')
+/** @format */
+const { resolve } = require('path');
+const { defineConfig } = require('@vue/cli-service');
 module.exports = defineConfig({
-  transpileDependencies: true
-})
+  transpileDependencies: true,
+  publicPath: '.',
+  css: {
+    loaderOptions: {
+      scss: {
+        // additionalData: `@import "${resolve('/src/styles/mixins/index.scss')}";`, // less前置
+      },
+    },
+  },
+  chainWebpack: (config) => {
+    config.resolve.alias.set('@src', resolve('src'));
+  },
+  devServer: {
+    proxy: {
+      '/screenAdmin': { target: 'http://124.91.150.6:10904', changeOrigin: true },
+    },
+  },
+});
